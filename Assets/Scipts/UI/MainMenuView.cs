1769bcb1371021dc4230d38e04635b1eabaf3c1a
Ralf Zeilstra <ralfzeils@gmail.com>
2022-12-11 12:31:55 +0100

    Setting Menu

--- Assets/Scipts/UI/MainMenuView.cs
+++ Assets/Scipts/UI/MainMenuView.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuView : View
 {
+    [SerializeField] private Button playButton;
     [SerializeField] private Button settingsButton;
 
     public override void Initialize()
     {
+        playButton.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
         settingsButton.onClick.AddListener(() => ViewManager.Show<SettingsMenuView>());
     }
 
--- Assets/Scipts/UI/SettingsMenuView.cs
+++ Assets/Scipts/UI/SettingsMenuView.cs
@@ -1,14 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SettingsMenuView : View
 {
+    [SerializeField] private Toggle fullscreenToggle;
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    [SerializeField] private TMP_Dropdown graphicsDropdown;
+    [SerializeField] private Slider volumeSlider;
     [SerializeField] private Button backButton;
+
+    [SerializeField] private AudioMixer audioMixer;
+    Resolution[] resolutions;
 
     public override void Initialize()
     {
+        //Add the resolution options to the resolution dropdown
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @" + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        //Set all listeners for the UI elements
+        resolutionDropdown.onValueChanged.AddListener(delegate { SetResolution(resolutionDropdown.value);});
+        fullscreenToggle.onValueChanged.AddListener(delegate { SetFullscreen(fullscreenToggle.isOn); });
+        graphicsDropdown.onValueChanged.AddListener(delegate { SetQuality(graphicsDropdown.value); });
+        volumeSlider.onValueChanged.AddListener(delegate { SetVolume(volumeSlider.value); });
         backButton.onClick.AddListener(() => ViewManager.ShowLast());
+    }
+
+    private void SetResolution ( int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    private void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeMaster", volume);
+    }
+
+    private void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    private void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
