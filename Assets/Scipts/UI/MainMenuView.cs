38d6f062445111378b292210b8b40bdd5632f219
Ralf Zeilstra <ralfzeils@gmail.com>
2022-12-11 15:06:13 +0100

    Added a panel to Settings menu

--- Assets/Scipts/UI/MainMenuView.cs
+++ Assets/Scipts/UI/MainMenuView.cs
@@ -8,11 +8,13 @@ public class MainMenuView : View
 {
     [SerializeField] private Button playButton;
     [SerializeField] private Button settingsButton;
+    [SerializeField] private Button quitButton;
 
     public override void Initialize()
     {
         playButton.onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
         settingsButton.onClick.AddListener(() => ViewManager.Show<SettingsMenuView>());
+        quitButton.onClick.AddListener(() => Application.Quit());
     }
 
     
